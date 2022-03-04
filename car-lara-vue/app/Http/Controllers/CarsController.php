0bc1e24f24576916d2371593f6cd61f55415041b
Obed Mulumba <ommartiste007@gmail.com>
2022-03-04 09:41:57 +0000

    Update CarsController.php

--- car-lara-vue/app/Http/Controllers/CarsController.php
+++ car-lara-vue/app/Http/Controllers/CarsController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class CarsController extends Controller
 {
-    //
+    public function storeCar(Request $request) {
+        $car = new Cars();
+        $car->make = $request->make;
+        $car->model = $request->model;
+        $car->save();
+
+        return $car;
+    }
 }
